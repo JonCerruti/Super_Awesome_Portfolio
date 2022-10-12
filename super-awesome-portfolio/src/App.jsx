e2c580a97420630c82681ac3b127886cd059cd77
Jon <jonathancerruti@gmail.com>
2022-10-12 02:43:11 -0700

    import jsx files into app.jsx

--- super-awesome-portfolio/src/App.jsx
+++ super-awesome-portfolio/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Header from './components/header/header'
+import Nav from './components/nav/nav'
+import About from './components/about/about'
+import Experience from './components/experience/experience'
+import Contact from './components/contacts/contact'
+import Footer from './components/footer/footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+        < Header />
+        < Nav />
+        < About/>
+        < Experience />
+        < Contact />
+        < Footer />
+    </>
   )
 }
 
