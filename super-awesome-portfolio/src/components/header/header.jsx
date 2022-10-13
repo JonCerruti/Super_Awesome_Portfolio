5f765ba6f0e34d74e02721e4cdc1308460289e71
Jon <jonathancerruti@gmail.com>
2022-10-13 02:10:44 -0700

    added general css and started building header and importing into react app

--- super-awesome-portfolio/src/components/header/header.jsx
+++ super-awesome-portfolio/src/components/header/header.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/images/profile-pic.jpg'
+import Family from '../../assets/images/family.jpg'
+import Football from '../../assets/images/football_with_mags.JPG'
 
 const header = () => {
   return (
-    <div>Header</div>
+   <header>
+      <div className="container header__container">
+        <h5>Hello I am</h5>
+        <h1>Jonathan Cerruti</h1>
+        <h5 className="text-light">Full Stack Web Developer</h5>
+        <CTA />
+
+        <div className='me'>
+          <img src={ME} alt= 'me' />
+        </div>
+        <div className='family'>
+          <img src={Family} alt= 'family' />
+        </div>
+        <div className='football'>
+          <img src={Football} alt= 'football' />
+        </div>
+        <a href='#contact' className=''scroll__down> Scroll Down </a>
+
+        
+
+      </div>
+   </header>
   )
 }
 
---
+++ super-awesome-portfolio/src/components/header/headerSocials.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const headerSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href='https://www.linkedin.com/in/jonathan-cerruti-a1b8a41b9/'target="_blank"></a>
+        <a href='https://github.com/JonCerruti' target="_blank"></a>
+    </div>
+  )
+}
+
+export default headerSocials
